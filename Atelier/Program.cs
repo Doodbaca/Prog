dda1ad8a03a0337890a480acb78597e6914298a2
biema1930528 <54861100+biema1930528@users.noreply.github.com>
2019-10-21 18:41:08 -0400

    Add files via upload

---
+++ Atelier/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nbSaisie = 0;
+            int valeur = 0;
+            
+            Console.WriteLine("entrez votre nombre");
+            nbSaisie = Convert.ToInt32(Console.ReadLine());
+            valeur = nbSaisie;
+            for(int i=1; i <= nbSaisie; i++)
+            {
+                valeur = valeur*i;
+            }
+            Console.WriteLine("valeur=" +valeur);
+            Console.ReadKey();
+        }
+    }
+}
